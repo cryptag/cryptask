c589e972f02f08008dc96edcdd2934ecc982abba
Steve Phillips <steve@tryingtobeawesome.com>
2016-06-27 00:47:19 -0700

    Search box placeholder: enter tags (plural), not tag

--- app/components/SearchForm.js
+++ app/components/SearchForm.js
@@ -6,7 +6,7 @@ export default class SearchForm extends Component {
       <form role="form" className="form" onSubmit={this.props.executeSearch}>
 
         <div className="row col-sm-6">
-          <input className="form-control search-box" type="text" onChange={this.props.onChangeTagValue} placeholder="enter tag" />
+          <input className="form-control search-box" type="text" onChange={this.props.onChangeTagValue} placeholder="enter tags" />
         </div>
         <div className="row">
           <div className="col-sm-2">
