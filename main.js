16e74890f55109a022ae0e6f72da44efed678bfc
Steve Phillips <steve@tryingtobeawesome.com>
2016-06-27 00:19:46 -0700

    BrowserWindow: 1024x600 -> 1280x800

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1024, height: 600})
+  mainWindow = new BrowserWindow({width: 1280, height: 800})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
