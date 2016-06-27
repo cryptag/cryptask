b0cc6da36ba54fa86007adca703c616488b4e2f9
Steve Phillips <steve@tryingtobeawesome.com>
2016-06-27 00:18:21 -0700

    Results table now fixed width, proper width per column

--- app/components/ResultList.js
+++ app/components/ResultList.js
@@ -10,10 +10,10 @@ export default class ResultList extends Component {
       <table role="table" className="table table-striped">
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Descriptions</th>
-            <th>Assignees</th>
-            <th>Tags</th>
+            <th className="col-sm-3">Title</th>
+            <th className="col-sm-6">Descriptions</th>
+            <th className="col-sm-1">Assignees</th>
+            <th className="col-sm-2">Tags</th>
           </tr>
         </thead>
         <tbody>
